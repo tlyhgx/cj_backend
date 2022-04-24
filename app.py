e3ea0969a2886afe8626617405f61887e73fc520
hww <1804597821@qq.com>
2022-04-24 11:13:13 +0800

    add app

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
 from distutils.log import debug
-from flask import Flask
+from os import lseek
+from flask import Flask,jsonify
 
 app=Flask(__name__)
 
 @app.route('/')
 def index():
     return "Hello world!"
+
+@app.route('/hello')
+def say_hello():
+    return jsonify({"message":"hello world"})
 
 if __name__=="__main__":
     app.run(debug=True)
